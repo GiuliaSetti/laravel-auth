cb312cfd9ca41d8890000b143d0dd1ce23ce6aab
GiuliaSetti <giulia.setti96@gmail.com>
2023-05-23 18:21:10 +0200

    feat: destroy()

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -25,6 +25,43 @@
     </div>
     <div class="button my-2">
         <a class="btn btn-primary" href="{{route('admin.projects.edit', $project->slug)}}">EDIT</a>
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteProject">
+            DELETE PROJECT
+        </button>
+    </div>
+
+    
+
+    <div class="modal" id="deleteProject" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h4 class="modal-title">Delete Project</h4>
+                </div>
+
+                <div class="modal-body">
+                   Are you sure? You cannot reverse the action.
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CLOSE</button>
+
+                    <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <button class="btn btn-danger" type="submit">DELETE</button>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div> 
+
+    <div class="m-5">
+        <a href="{{route('admin.projects.index')}}">Go back to the projects page</a>
     </div>
 </div>
 
